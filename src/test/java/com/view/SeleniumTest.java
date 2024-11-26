ef314f5f0f284120b0682e2364cc66c387c57bdd
ciscodeto <netofcisco@gmail.com>
2024-11-25 23:20:47 -0300

    feat: chromeDriver works well

---
+++ src/test/java/com/view/SeleniumTest.java
@@ -0,0 +1,19 @@
+package com.view;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumTest {
+    @Test
+    @DisplayName("Should open and close chrome browser")
+    void shouldOpenAndCloseChromeBrowser() throws InterruptedException {
+// sets the driver path
+        System.setProperty("webdriver.chrome.driver","src\\test\\resources\\drivers\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver(); // creates a driver to interact with the browser
+        driver.get("https://www.google.com"); // request the page
+        Thread.sleep(1000); // waits for 1s.
+        driver.quit(); // quits all WebDriver instances and closes the browser
+    }
+}
