89edd8acde3aa3e24a4cf538e75b1594307aee7c
ciscodeto <netofcisco@gmail.com>
2024-11-25 23:47:02 -0300

    fix: test chromedrive works well

--- src/test/java/com/view/SeleniumTest.java
+++ src/test/java/com/view/SeleniumTest.java
@@ -4,16 +4,37 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.nio.file.Paths;
 
 public class SeleniumTest {
     @Test
-    @DisplayName("Should open and close chrome browser")
+    @DisplayName("Should open and close Chrome browser")
     void shouldOpenAndCloseChromeBrowser() throws InterruptedException {
-// sets the driver path
-        System.setProperty("webdriver.chrome.driver","src\\test\\resources\\drivers\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver(); // creates a driver to interact with the browser
-        driver.get("https://www.google.com"); // request the page
-        Thread.sleep(1000); // waits for 1s.
-        driver.quit(); // quits all WebDriver instances and closes the browser
+        // Configura o caminho para o ChromeDriver
+        System.setProperty("webdriver.chrome.driver",
+                Paths.get("src", "test", "resources", "drivers", "chromedriver.exe").toString());
+
+        // Configurações adicionais do Chrome
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+        // Inicializa o WebDriver
+        WebDriver driver = new ChromeDriver(options);
+
+        try {
+            // Acessa a página do Google
+            System.out.println("Abrindo o navegador...");
+            driver.get("https://www.google.com");
+            System.out.println("Título da página: " + driver.getTitle());
+
+            // Aguarda 1 segundo para visualização
+            Thread.sleep(1000);
+        } finally {
+            // Fecha o navegador
+            driver.quit();
+            System.out.println("Navegador fechado.");
+        }
     }
 }
