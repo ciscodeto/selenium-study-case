665ad128ff935081f4fd6900c2d9906f6635fd83
brunor36 <bruno.ribaldo@aluno.ifsp.edu.br>
2024-12-04 14:33:45 -0300

    testSaveAppointmentWithPastDate

--- src/test/java/com/view/PacientPageTest.java
+++ src/test/java/com/view/PacientPageTest.java
@@ -66,4 +66,38 @@ public class PacientPageTest extends BaseTest {
             fail("Erro ao verificar a criação do arquivo de relatório.");
         }
     }
+    @Test
+    @DisplayName("Tentar salvar atendimento com data passada para o paciente 'bob.brown654'")
+    void testSaveAppointmentWithPastDate() {
+        driver.get(pacienteUrl);
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        WebElement patientNameField = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id='root']/div/div[1]/label/input")));
+        patientNameField.sendKeys("bob.brown654");
+
+        WebElement patientNumberField = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id='root']/div/div[2]/input")));
+        patientNumberField.sendKeys("13690000");
+
+        WebElement confirmButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id='root']/div/div[2]/button")));
+        confirmButton.click();
+
+        WebElement dateField = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id='root']/div/div[3]/label/input")));
+        dateField.sendKeys("2023-01-01");
+
+        WebElement timeField = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id='root']/div/div[4]/label/input")));
+        timeField.sendKeys("10:30");
+
+        WebElement specialtyField = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id='root']/div/div[5]/label/input")));
+        specialtyField.sendKeys("Otorrinolaringologista");
+
+        WebElement saveButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id='root']/div/button[1]")));
+        saveButton.click();
+
+        WebElement errorMessage = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div/div/p")));
+
+        String errorText = errorMessage.getText();
+        assertEquals("A data da consulta deve ser maior que a data atual.", errorText,
+                "A mensagem de erro não é a esperada!");
+    }
 }
