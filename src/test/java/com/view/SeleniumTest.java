070df455fe830b12928054663ac7f53921536f50
brunor36 <bruno.ribaldo@aluno.ifsp.edu.br>
2024-12-03 23:29:48 -0300

    BeforeEach DoctorUrl

--- src/test/java/com/view/SeleniumTest.java
+++ src/test/java/com/view/SeleniumTest.java
@@ -326,38 +326,48 @@ public class SeleniumTest {
                     currentUrl, "A URL atual não é a esperada!");
         }
     }
-    @Test
-    @DisplayName("Tentar salvar atendimento com médico não existente verificando a lista na página Admin")
-    void testSaveAppointmentWithNonExistentDoctor() {
 
-        driver.get("https://sitetc1kaykywaleskabreno.vercel.app/admin");
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        List<WebElement> doctorList = wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath("//ul/li")));
-        boolean doctorExists = doctorList.stream()
-                .anyMatch(doctor -> doctor.getText().equals("Dr. Inexistente"));
-        assertFalse(doctorExists, "O médico 'Dr. Inexistente' foi encontrado na lista, mas não deveria existir.");
+    @Nested
+    @DisplayName("Doctor Page Test")
+    class DoctorPageTest {
+        private final String doctorUrl = "https://sitetc1kaykywaleskabreno.vercel.app/medico";
 
-        driver.get("https://sitetc1kaykywaleskabreno.vercel.app/medico");
+        @BeforeEach
+        void DoctorUrlSetUp() {
+            driver.get(doctorUrl);
+        }
 
-        WebElement doctorNameField = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id='root']/div/div[1]/label/input")));
-        doctorNameField.sendKeys("Dr. Inexistente");
+        @Test
+        @DisplayName("Tentar salvar atendimento com médico não existente verificando a lista na página Admin")
+        void testSaveAppointmentWithNonExistentDoctor() {
 
-        WebElement doctorSelectField = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id='root']/div/div[2]/label/select/option[2]")));
-        doctorSelectField.click();
+            driver.get("https://sitetc1kaykywaleskabreno.vercel.app/admin");
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+            List<WebElement> doctorList = wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath("//ul/li")));
+            boolean doctorExists = doctorList.stream()
+                    .anyMatch(doctor -> doctor.getText().equals("Dr. Inexistente"));
+            assertFalse(doctorExists, "O médico 'Dr. Inexistente' foi encontrado na lista, mas não deveria existir.");
 
-        WebElement patientNumberField = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id='root']/div/div[3]/input")));
-        patientNumberField.sendKeys("13690000");
+            driver.get(doctorUrl);
 
-        WebElement saveButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id='root']/div/div[3]/button")));
-        saveButton.click();
+            WebElement doctorNameField = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id='root']/div/div[1]/label/input")));
+            doctorNameField.sendKeys("Dr. Inexistente");
 
-        WebElement observationsField = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id='root']/div/div[4]/label/textarea")));
-        observationsField.sendKeys("Paciente liberado");
+            WebElement doctorSelectField = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id='root']/div/div[2]/label/select/option[2]")));
+            doctorSelectField.click();
 
-        String currentUrl = driver.getCurrentUrl();
-        assertNotEquals("https://sitetc1kaykywaleskabreno.vercel.app/confirmacao", currentUrl,
-                "A URL não deveria ser de confirmação de atendimento salvo!");
+            WebElement patientNumberField = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id='root']/div/div[3]/input")));
+            patientNumberField.sendKeys("13690000");
+
+            WebElement saveButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id='root']/div/div[3]/button")));
+            saveButton.click();
+
+            WebElement observationsField = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id='root']/div/div[4]/label/textarea")));
+            observationsField.sendKeys("Paciente liberado");
+
+            String currentUrl = driver.getCurrentUrl();
+            assertNotEquals("https://sitetc1kaykywaleskabreno.vercel.app/confirmacao", currentUrl,
+                    "A URL não deveria ser de confirmação de atendimento salvo!");
+        }
     }
-
-
 }
