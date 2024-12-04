6e700914142c240ceb33424c7c3694a68615ba16
brunor36 <bruno.ribaldo@aluno.ifsp.edu.br>
2024-12-04 17:13:22 -0300

    Adjusting DoctorLoginWithWrongUserAndCorrectPassword

--- src/test/java/com/view/LoginPageTest.java
+++ src/test/java/com/view/LoginPageTest.java
@@ -185,10 +185,10 @@ public class LoginPageTest extends BaseTest {
         passwordField.sendKeys("DianaPass123!");
         loginButton.click();
 
-        wait.until(ExpectedConditions.not(ExpectedConditions.urlToBe("https://sitetc1kaykywaleskabreno.vercel.app/medico")));
+        wait.until(ExpectedConditions.urlToBe("https://sitetc1kaykywaleskabreno.vercel.app/login"));
 
         String currentUrl = driver.getCurrentUrl();
-        assertNotEquals("https://sitetc1kaykywaleskabreno.vercel.app/medico",
+        assertEquals("https://sitetc1kaykywaleskabreno.vercel.app/medico",
                 currentUrl, "A URL atual não é a esperada!");
     }
     @Test
